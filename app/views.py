762010a75476e525261e3843f79132600da88bda
Roberto Alvarez <ralvarez@airata.net>
2024-11-24 14:58:44 -0300

    Se agrega login/logout funcionando.
    Se modificó:
    
    views.py: para eliminar una importación innecesaria.
    
    header.html: para cambiar url de logout para hacer funcionar el botón salir, y para modificar el funcionamiento de las negritas en el header, se agregan las condiciones por request.path para mostrar correctamente el indicador de página en el que se encuentra.

--- app/views.py
+++ app/views.py
@@ -3,7 +3,6 @@
 from django.shortcuts import redirect, render
 from .layers.services import services
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
 
 def index_page(request):
     return render(request, 'index.html')
